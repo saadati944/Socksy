e47bc93347911c333ba236d94081a6df7b775201
saadati944 <saadati944@gmail.com>
2022-11-24 16:17:14 +0330

    Create NetHelper class

--- Socksy.Core/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Socksy.Core
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Socksy.Core/Usings.cs
@@ -0,0 +1,4 @@
+﻿global using System;
+global using System.Net;
+global using System.Net.Sockets;
+global using System.Threading.Tasks;
